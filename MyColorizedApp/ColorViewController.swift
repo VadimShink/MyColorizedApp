3d7f8a42b5d53b149d759f43d97718ee0f5fe6da
Rigel <vadim_shink@icloud.com>
2022-07-07 18:28:59 +0300

    Add main screen

--- MyColorizedApp/ColorViewController.swift
+++ MyColorizedApp/ColorViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ColorViewController: UIViewController {
 
     @IBOutlet weak var rgbView: UIView!
     
---
+++ MyColorizedApp/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ColorViewController.swift
+//  MyColorizedApp
+//
+//  Created by Vadim Shinkarenko on 07.07.2022.
+//
+
+import Foundation
+import UIKit
+
+
+class MainViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
