6841ee0dc3403d452587e73c055c5c282009b0e2
Rigel <vadim_shink@icloud.com>
2022-07-07 18:59:16 +0300

    Add functionality

--- MyColorizedApp/MainViewController.swift
+++ MyColorizedApp/MainViewController.swift
@@ -5,9 +5,7 @@
 //  Created by Vadim Shinkarenko on 07.07.2022.
 //
 
-import Foundation
 import UIKit
-
 
 class MainViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
